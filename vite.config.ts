d36f6b17f7de617d6579934305fbf8510eb78b9a
Max Yarmakovich <yamaksim@sas.upenn.edu>
2024-12-27 15:19:31 -0600

    test3

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Maksim125.github.io/',
+  base: '/self/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
